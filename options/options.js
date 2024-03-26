7da9340bf79b41658a38f448eafc50ee639a84be
Crowdbotics <team@crowdbotics.com>
2024-03-26 14:33:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shaquille-riston-ap-47282",
+  url: "https://shaquille-riston-ap-47282.botics.co",
+  api: "https://shaquille-riston-ap-47282.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
